a929e8aece3261cc0d4344ea2ddec6c3978dc753
aliza-rizwan <aliza.rizwan@ontariotechu.net>
2025-10-04 01:40:28 -0400

    Create Product.java

---
+++ SwingMVC/src/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+    private String upc;
+    private String name;
+    private double price;
+
+    public Product(String upc, String name, double price) {
+        this.upc = upc;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getUpc() { return upc; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return name + " - $" + String.format("%.2f", price);
+    }
+}
