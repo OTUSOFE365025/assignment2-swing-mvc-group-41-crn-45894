15e9a1302ddecd4a49ac21dacbea9adc32472682
aliza-rizwan <aliza.rizwan@ontariotechu.net>
2025-10-04 01:49:05 -0400

    Update Controller.java

--- SwingMVC/src/Controller.java
+++ SwingMVC/src/Controller.java
@@ -1,43 +1,24 @@
-import javax.swing.JOptionPane;
- 
+import java.util.List;
+
 public class Controller {
-	 private Model model;
-	 private View view;
-	 
-	 public Controller(Model m, View v) {
-	  model = m;
-	  view = v;
-	  initView();
-	 }
-	 
-	 public void initView() {
-	  view.getFirstnameTextfield().setText(model.getFirstname());
-	  view.getLastnameTextfield().setText(model.getLastname());
-	 }
-	 
-	 public void initController() {
-	  view.getFirstnameSaveButton().addActionListener(e -> saveFirstname());
-	  view.getLastnameSaveButton().addActionListener(e -> saveLastname());
-	  view.getHello().addActionListener(e -> sayHello());
-	  view.getBye().addActionListener(e -> sayBye());
-	 }
-	 
-	 private void saveFirstname() {
-	  model.setFirstname(view.getFirstnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Firstname saved : " + model.getFirstname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void saveLastname() {
-	  model.setLastname(view.getLastnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Lastname saved : " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayHello() {
-	  JOptionPane.showMessageDialog(null, "Hello " + model.getFirstname() + " " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayBye() {
-	  System.exit(0);
-	 }
-	 
+    private CashRegister model;
+    private View view;
+
+    public Controller(CashRegister m, View v) {
+        model = m;
+        view = v;
+        initView();
+    }
+
+    public void initView() {
+        view.updateDisplay(model.getScannedItems(), model.getSubtotal());
+    }
+
+    // Handles UPC scans from the Scanner window
+    public void handleScan(String upc) {
+        model.addItemByUPC(upc);
+        List<Product> items = model.getScannedItems();
+        double subtotal = model.getSubtotal();
+        view.updateDisplay(items, subtotal);
+    }
 }
