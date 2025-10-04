11cc2e8fb9d8ef96459b5a9dfb2dfc2616598405
aliza-rizwan <aliza.rizwan@ontariotechu.net>
2025-10-04 02:30:40 -0400

    Update MySwingMVCApp.java

--- SwingMVC/src/MySwingMVCApp.java
+++ SwingMVC/src/MySwingMVCApp.java
@@ -1,22 +1,28 @@
 public class MySwingMVCApp {
 
     public static void main(String[] args) {
-       
+
         Model m = new Model("Sylvain", "Saurel");
         View v = new View("MVC with SSaurel");
-        Controller c = new Controller(m, v);
-        c.initController();
+        Controller c1 = new Controller(m, v);
+        c1.initController();
+
 
         CashRegister register = new CashRegister("products.txt");
 
+        Model m2 = new Model("Aliza", "Rizwan");
         View displayView = new View("Cash Register");
+        Controller c2 = new Controller(m2, displayView);
+        c2.initController();
+
         displayView.createDisplayArea();
 
-        c.setCashRegister(register, displayView);
+        c2.setCashRegister(register, displayView);
 
-        new Scanner(c);
-
-        System.out.println("Program started.");
-        System.out.println("Press the 'Scan' button to simulate scanning a product.");
+        new Scanner(c2);
+        
+        System.out.println("Program started successfully.");
+        System.out.println("You can click 'Scan' to simulate scanning products.");
+        System.out.println("Both 'Hello' buttons now display the correct names.");
     }
 }
